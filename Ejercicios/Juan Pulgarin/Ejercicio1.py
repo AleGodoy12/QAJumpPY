ace747b5d6f2cd6d645bc5b27c29be498c4cb413
jfpulgarin08 <juanfelipe.pulgarin@arbusta.net>
2022-12-22 14:56:08 -0500

    subo ejercicios

---
+++ Ejercicios/Juan Pulgarin/Ejercicio1.py
@@ -0,0 +1,63 @@
+"1. Escribir un programa que muestre por pantalla la cadena de texto."
+print ("Hola bienvenidos")
+"Solución JAVA"
+"""
+System.out.print(Hola bienvenidos)
+"""
+"2. Declare una variable que en su valor contenga un digito como cadena de texto"
+saludo = "Hola mundo"
+print (saludo)
+"Solución JAVA"
+"""
+Usando var
+Usando let cuando sea algo que puede cambiar 
+Usando const cuando sea un constante
+usando solo una letra o una cadena de texto
+saludo= "Hola mundo" 
+o
+var saludo= "Hola mundo"
+"""
+"3.  Declare una variable con su nombre, otra con su apellido y concatenelas para mostrar por consola."
+n = "Juan"
+a = "Pulgarin"
+print (n +" "+ a)
+"Solución JAVA"
+"""
+var n = "Juan"
+var apellido = "Pulgarin"
+print (n +" "+ apellido)
+"""
+"4. Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido."
+
+nombre = input ("Hola bienvenidx digita tu nombre: ")
+print ("Hola " + nombre)
+"Solución JAVA"
+"""
+var input = document.createElement("Hola bienvenidx digita tu nombre: ");
+input.setAttribute('type', 'text');
+var parent = document.getElementById("parentDiv");
+parent.appendChild(input);
+"""
+"5. Declare una variable cuyo valor sea el numero 2, otra variable cuyo valor sea el numero 10 y realice las operaciones aritmeticas basicas (suma, resta, multiplicacion y division), mostrando el resultado de cada una de ellas por pantalla."
+n1=2
+n2=10
+"Suma"
+print (n1+n2)
+"resta"
+print (n1-n2)
+"Multiplicacion"
+print (n1*n2)
+"División"
+print (n2/n1)
+"""
+n1=2
+n2=10
+"Suma"
+print (n1+n2)
+"resta"
+print (n1-n2)
+"Multiplicacion"
+print (n1*n2)
+"División"
+print (n2/n1)
+"""
