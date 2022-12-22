f8532a1d2820e5775731542348143ddd22901a1b
nataliasanabria <natalia.sanabria@arbusta.net>
2022-12-22 15:32:28 -0300

    ejercicio 1

---
+++ Ejercicios/Natalia Sanabria/Ejercicios1.py
@@ -0,0 +1,69 @@
+#1
+print ("Hola, este es mi primer programa")
+#class saludo{
+# public static void main(String args[]){
+# System.out.println("Hola, este es mi primer programa");
+#    }
+# }
+
+#2
+digitotexto="1999"
+print(digitotexto)
+#public class texto{ 
+# public static void main(String args[]){
+# String cadena="1999";
+# System.out.println(cadena);  
+#  }
+# }
+
+#3
+nombre="Natalia"
+apellido="Sanabria"
+print(nombre + " " +apellido)
+#public class datos{
+# public static void main(String args[]){
+# String nombre="Natalia";
+# String apellido="Sanabria";
+# System.out.println(nombre + apellido);
+# }
+#  }
+
+#4
+nombre=input("Ingrese su nombre:")
+print("Hola "+ nombre)
+#public class saludoUser{
+# public static void main(String args[]){
+# Scanner entrada= new Scanner(System.in);
+# String nombre;
+# System.out.println("Ingrese su nombre: "); 
+# nombre= entrada.nextLine(); 
+# System.out.println("Hola" + nombre);
+# }
+#  }
+
+#5
+nro1=10
+nro2=2
+print("Suma:")
+print(int(nro1+nro2))
+print("Resta:")
+print(int(nro1-nro2))
+print("Multiplicación:")
+print(int(nro1*nro2))
+print("División:")
+print(int(nro1/nro2))
+#public class operaciones{
+# public static void main(String args[]){
+# int nro1=10;
+# int nro2=2;
+# int suma,resta,div,multi;
+# suma= nro1+nro2;
+# resta= nro1-nro2;
+# div= nro1/nro2;
+# multi= nro1*nro2;
+# System.out.println("Suma:" +suma);
+# System.out.println("Resta:" + resta);
+# System.out.println("Division: " + div);
+# System.out.println("Multiplicacion: " + multi);
+# }
+#  }
