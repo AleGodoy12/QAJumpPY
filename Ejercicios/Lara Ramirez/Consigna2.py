aa61cb93daa0728be509f80c4eec5565ce5229c0
unknown <yael.ramirez@arbusta.net>
2023-01-03 09:20:12 -0300

    Nuevo ejercicio

---
+++ Ejercicios/Lara Ramirez/Consigna2.py
@@ -0,0 +1,35 @@
+# Agregar un archivo .py en la carpeta con tu nombre con la resolucion de cada una de las consignas. 
+# Recorda investigar y comentar la resolucion en java
+
+# 1. Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
+
+# 2. Con el programa anterior, utilice todos los metodos vistos en clases, mostrando cada uno con un print. 
+
+# 3. Escribir un programa que almacene una lista de ingredientes para hacer galletas y convierta dicha lista en una tupla.
+
+
+# 1. 
+
+materias = ['Matemáticas', 'Física', 'Química', 'Historia', 'Lengua']
+print = (materias)
+
+# 2.
+
+materias = ['Matemáticas', 'Física', 'Química', 'Historia', 'Lengua']
+
+print (materias[3])
+print (materias + ['Contabilidad', 'Inglés'])
+print (materias.append ("Educación física"))
+print (materias [:2]= ['MATEMÁTICAS', 'FÍSICA'])
+print (materias [:2]= [])
+print (materias.pop ())
+print (materias.pop ('Matemáticas'))
+print (materias.count("Física"))
+print (materias.index("Química"))
+print (len(materias))
+print (materias =[])
+
+# 3. 
+
+receta = ['harina', 'azúcar', 'vainilla', 'leche', 'manteca', 'huevo']
+tuple(receta)
