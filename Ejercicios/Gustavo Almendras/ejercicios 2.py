b531fa5884d34ef2e7699414f5770113ea0c2beb
gustavo.almendras@arbusta.net <gustavo.almendras@arbusta.net>
2023-01-04 16:59:40 -0300

    Agrego ejercicios

---
+++ Ejercicios/Gustavo Almendras/ejercicios 2.py
@@ -0,0 +1,23 @@
+#1 
+#Materias =  ['Matemáticas','Física','Química','Historia','Lengua']
+#print(Materias)
+
+#2
+#Append
+#Materias.append("Ingles")
+
+#Len
+#print(len(Materias))
+
+#Pop
+#Materias.pop()
+
+#Count
+#print(Materias.count("Matemáticas"))
+
+#Index
+#print(Materias.index("Química"))
+
+#3
+#galletas = ["Harina","Mantequilla","Azúcar","Bicarbonato","Esencia de vainilla"]
+#print(tuple(galletas))
---
+++ Ejercicios/Gustavo Almendras/ejercicios 3.py
@@ -0,0 +1,16 @@
+"""
+x = 0
+while x <= 30:
+    print("El valor del bucle es: ", x)
+    x += 1
+    if x == 4 or x == 6 or x == 10:
+        print("Se salto el valor: ", x)
+        x += 1
+    if x == 15:
+        break
+print("Se rompio la ejecución cuando x valia: ",x)
+"""
+"""num = 7
+for i in range(num,700,100):
+    print(i)"""
+
