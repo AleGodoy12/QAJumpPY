a7eeec729af9ec31d62e430670756f0ec6146762
Arbusta <kpaiva@arbusta.net>
2023-01-06 09:42:26 -0300

    subo ejercicio

--- Ejercicios/Karina Paiva/Ejercicio1.py
+++ Ejercicios/Karina Paiva/Ejercicio1.py
@@ -19,13 +19,30 @@
 
 #5. Declare una variable cuyo valor sea el numero 2, otra variable cuyo valor sea el numero 10 y realice las operaciones aritmeticas basicas
 # (suma, resta, multiplicacion y division), mostrando el resultado de cada una de ellas por pantalla. 
-num1 = 2
-num2 = 10
-print ("suma: ") 
-print(int(num1+num2))
-print("multiplicacion: ") 
-print (int(num1*num2))
+#num1 = 2
+#num2 = 10
+#print ("suma: ") 
+#print(int(num1+num2))
+#print("multiplicacion: ") 
+#print (int(num1*num2))
 #print("resta: ") 
 #print (int(num2-num1))
 #print("división: ")
 #print (int(num1/num2))
+
+#ingredientes = ['Harina', 'huevo', 'leche', 'manteca']
+#tuple(ingredientes)
+#print(ingredientes)
+
+#Nombre = input("ingrese su nombre")
+#Apellido = input ("ingresa su apellido")
+#print("Hola " + Nombre + Apellido + " , Bienvenido!")
+
+#F = float(1,14)
+#print(F)
+
+#T = ("cadena de texto", 10, 0.12, "otra cadena", -4)
+#print (T)
+T = ("cadena de texto", 10, 0.12, "otra cadena", -4)
+T =()
+print (T)
---
+++ Ejercicios/Karina Paiva/Ejercicio2.py
@@ -0,0 +1,20 @@
+#1. Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
+Asignaturas = ["Matematicas", "fisica", "Química", "Historia", "Lengua"]
+#print(Asignaturas)
+#2. Con el programa anterior, utilice todos los metodos vistos en clases, mostrando cada uno con un print. 
+#Asignaturas[:3]=["MATEMATICAS", "FISICA", "QUIMICA"]
+#print(Asignaturas)
+
+#Asignaturas.append("Geografia")
+#print(Asignaturas)
+
+#len(Asignaturas)
+
+#Asignaturas.pop()
+#print(Asignaturas)
+
+#3. Escribir un programa que almacene una lista de ingredientes para hacer galletas y convierta dicha lista en una tupla.
+
+#ingredientes = ['Harina', 'huevo', 'leche', 'manteca']
+#tuple(ingredientes)
+#print(ingredientes)
